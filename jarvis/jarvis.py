3152acf37141b21ff11d65035468ca7d865715b1
namancristvats <motsgols19@gmail.com>
2020-06-17 21:49:54 +0530

    Update jarvis.py
    changing username and password

--- jarvis/jarvis.py
+++ jarvis/jarvis.py
@@ -92,7 +92,7 @@ if __name__=="__main__":
         elif 'open instagram' in query:
             path()
             Url_name()
-            login("crist_2503","Cynthiavats@123")
+            login("username","password")
             exit()
 
  
